e4fc3d4663701e22a5f02405a6d0f1ffc459b1ee
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-01-23 21:40:59 +0530

    #include<stdio.h> int main() {     int m,n,max,lcm;     scanf("%d%d",&m,&n);     max=m>n?m:n;     lcm=max;     for(int i=max;;i=i+max)     {          if(i%m==0 && i%n==0)          {              lcm=i;              break;          }     }     printf("%d",lcm);     return 0; }

---
+++ LCM.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{ 
+    int m,n,max,lcm;
+    scanf("%d%d",&m,&n); 
+    max=m>n?m:n;
+    lcm=max;
+    for(int i=max;;i=i+max)
+    {
+         if(i%m==0 && i%n==0)
+         {
+             lcm=i;
+             break;
+         }
+    }
+    printf("%d",lcm);
+    return 0;
+}
