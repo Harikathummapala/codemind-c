54ef3d4c68aafb8bb6dbfc334ae22a006846280e
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-04-28 21:35:26 +0530

    #include<stdio.h> int main() {     int n,sum=0,p;     scanf("%d",&n);     while(n!=0)     {         p=n%10;         if(p>sum)         {             sum=p;         }     n=n/10;     }     printf("%d",sum); }

---
+++ Largest_Digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,p;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        p=n%10;
+        if(p>sum)
+        {
+            sum=p;
+        }
+    n=n/10;
+    }
+    printf("%d",sum);
+}
