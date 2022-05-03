2cbf26c836d09a540628631bb4893c3fc4ebe5db
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-05-03 14:20:04 +0530

    #include<stdio.h> int main() {     int n, i,mini,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);
        }
        mini>arr[i];
        for(i=0;i<n;i++)
        {
            mini=arr[i];
        }
        printf("%d",mini);
        
    }

---
+++ minimum_element_of_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n, i,mini,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+    }
+    mini>arr[i];
+    for(i=0;i<n;i++)
+    {
+        mini=arr[i];
+    }
+    printf("%d",mini);
+    
+}
