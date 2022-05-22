6a0b110d5ade968767e28fb17ad185310be2e114
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-05-22 21:43:20 +0530

    #include<stdio.h> int main() {     int n,i,c=0,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);
        }
        for(i=1;i<(n-1);i++)
        {
            if(arr[i-1]%2==0 && arr[i+1]%2==0)
            {
                if(arr[i]%2!=0)
                {
                    c++;
                }
            }
        }
        printf("%d",c);
        
    }

---
+++ count_odd_numbers_between_even_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+    }
+    for(i=1;i<(n-1);i++)
+    {
+        if(arr[i-1]%2==0 && arr[i+1]%2==0)
+        {
+            if(arr[i]%2!=0)
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+    
+}
