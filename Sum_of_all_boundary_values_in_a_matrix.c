cb4a3f5e8ced3b4d759c654d84d6c4fb50d49a48
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-12 15:12:40 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[100][100],s=0,s1=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }
        for(i=0;i<r;i++)
        {
            for(j=0;j<c;j++)
            {
               
                    s+=arr[i][j];
                
            }
        }
        
        for(i=0;i<r;i++)
        {
            for(j=0;j<c;j++)
            {
                if(i==0 || j==0 || i==r-1 || j==c-1)
                {
                    s1+=arr[i][j];
                }
            }
        }
        printf("%d",s1);
    }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,arr[100][100],s=0,s1=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+   
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+           
+                s+=arr[i][j];
+            
+        }
+    }
+    
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==0 || j==0 || i==r-1 || j==c-1)
+            {
+                s1+=arr[i][j];
+            }
+        }
+    }
+    printf("%d",s1);
+}
+
