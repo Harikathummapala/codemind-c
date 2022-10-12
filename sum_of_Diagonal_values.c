e04def07d5d1c9f595fc1448bc26fc2fa98da9d1
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-12 14:58:31 +0530

    #include<stdio.h> int main() {     int i,j,r,c,arr[100][100],s=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }    for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i==j)             {                 s+=arr[i][j];                 }         }     }     j=r-1;     for(i=0;i<r;i++)     {         if(i!=j)         {             s=s+arr[i][j];         }         j--;     }     printf("%d",s); }

---
+++ sum_of_Diagonal_values.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,r,c,arr[100][100],s=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+   for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==j)
+            {
+                s+=arr[i][j];
+                }
+        }
+    } 
+    j=r-1;
+    for(i=0;i<r;i++)
+    {
+        if(i!=j)
+        {
+            s=s+arr[i][j];
+        }
+        j--;
+    }
+    printf("%d",s);
+}
